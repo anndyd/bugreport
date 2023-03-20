7e6b9d7e02d19ead3085d48b0e9bb3ecc6b71298
Dong Changliu <dongchangliu@outlook.com>
2023-03-20 20:08:16 +0800

    new modification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CapacitorHttp, HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'http-app';
+
+  blob: any;
+
+  constructor() {}
+
+  async onBtnClick() {
+    /**
+     * please replace the following url to a real picture url,
+     * and the url configured accept cross domain
+     */
+    const url = 'http://127.0.0.1:4200/assets/logo_yk.png';
+    const options = {
+      url,
+      responseType: 'blob',
+      connectTimeout: 60000,
+      readTimeout: 60000
+    } as HttpOptions;
+    console.log('-----------before get picture,,,,,,,,');
+    const res = await CapacitorHttp.get(options);
+    console.log('-----------after get picture,,,,,,,,' + res.data);
+    const base64 = 'data:image/jpeg;base64,' + res.data;
+    this.blob = base64;
+  }
+
 }
